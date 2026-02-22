ecc01abfc3d6e3ef26489debd738314b171c2011
Fauzi455 <fauzirhmn45@gmail.com>
2026-02-22 23:21:11 +0700

    Mengedit Route

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,19 @@ Route::get('/selamat', function () {
 Route::get('/about', function () {
     return '244107020067 - Mohamat Fauzi Rohman';
 });
+
+Route::get('/user/{name}', function ($name) {
+return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID $id";
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+return 'Nama saya '.$name;
+});
