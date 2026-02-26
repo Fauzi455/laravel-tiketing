0dbcde6f779ff844090a1ce9c9c1e68cfef7bfc0
Fauzi455 <fauzirhmn45@gmail.com>
2026-02-26 16:22:37 +0700

    Praktikum Eloquent ORM

--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,35 +1,37 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\LevelController;
 Route::get('/level', [LevelController::class, 'index']);
-
-use App\Http\Controllers\KategoriController;
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
 
-// 1. Home
-Route::get('/', [HomeController::class, 'index']);
+// use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\ProductController;
+// use App\Http\Controllers\UserController;
+// use App\Http\Controllers\SalesController;
 
-// 2. Products
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'food']);
-    Route::get('/beauty-health', [ProductController::class, 'beauty']);
-    Route::get('/home-care', [ProductController::class, 'home']);
-    Route::get('/baby-kid', [ProductController::class, 'baby']);
-});
+// // 1. Home
+// Route::get('/', [HomeController::class, 'index']);
 
-// 3. User
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+// // 2. Products
+// Route::prefix('category')->group(function () {
+//     Route::get('/food-beverage', [ProductController::class, 'food']);
+//     Route::get('/beauty-health', [ProductController::class, 'beauty']);
+//     Route::get('/home-care', [ProductController::class, 'home']);
+//     Route::get('/baby-kid', [ProductController::class, 'baby']);
+// });
 
-// 4. Penjualan
-Route::get('/sales', [SalesController::class, 'index']);
+// // 3. User
+// Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+// // 4. Penjualan
+// Route::get('/sales', [SalesController::class, 'index']);
