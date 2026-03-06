04f6aca1c33a24b883151be4b4febb458d683ee3
Fauzi455 <fauzirhmn45@gmail.com>
2026-03-06 13:44:22 +0700

    Praktikum Layouting AdminLTE

---
+++ POS/resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+      <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+  @include('layouts.footer')
+</div>
+<!-- ./wrapper -->
+</html>
