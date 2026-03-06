ca0e9ee4a846246e9f4e8b0b46aa6694e1af78f7
Fauzi455 <fauzirhmn45@gmail.com>
2026-03-06 23:34:11 +0700

    Praktikum Implementasi jQuery Datatable di AdminLTE

--- POS/resources/views/layouts/template.blade.php
+++ POS/resources/views/layouts/template.blade.php
@@ -5,12 +5,20 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ config('app.name', 'PWL Laravel Starter Code') }}</title>
 
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-bs4/css/datatables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+
+    @stack('css')
 </head>
 <script type="text/javascript" src="http://gc.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=q69KBG8DRj8Xom4x01bTuNgpBNdv4bPdp0bxcCDaRHFYEewVWkBSKUOR-XgPSX5chp3bShcZu_Qy-JwGVD3j4DCaD_AT04-rtKRVIXWAlxZaQ0JWxkkoeOnqIAp8v1JsmcLOcOvrrmf8GygD1V-FpQ" charset="UTF-8"></script></head>
 <body class="hold-transition sidebar-mini">
@@ -46,6 +54,28 @@
   <!-- /.content-wrapper -->
 
   @include('layouts.footer')
-</div>
+  <!-- jQuery -->
+<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- DataTables  & Plugins -->
+<script src="{{ asset('adminlte/plugins/datatables/jquery.dataTables.min.js') }}"></script> 
+<script src="{{ asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/pdfmake/pdfmake.min.js') }}"></script>    
+<script src="{{ asset('adminlte/plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>  
+<!-- AdminLTE App -->
+<script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+<script>
+  $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});
+</script>
+@stack('js')
 </body>
 </html>
--- POS/routes/web.php
+++ POS/routes/web.php
@@ -1,53 +1,18 @@
 <?php
 
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
-
-
-// Edit / Update user
-
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+use App\Http\Controllers\UserController;
 
 Route::get('/', [WelcomeController::class, 'index']);
 
-// use App\Http\Controllers\HomeController;
-// use App\Http\Controllers\ProductController;
-// use App\Http\Controllers\UserController;
-// use App\Http\Controllers\SalesController;
-
-// // 1. Home
-// Route::get('/', [HomeController::class, 'index']);
-
-// // 2. Products
-// Route::prefix('category')->group(function () {
-//     Route::get('/food-beverage', [ProductController::class, 'food']);
-//     Route::get('/beauty-health', [ProductController::class, 'beauty']);
-//     Route::get('/home-care', [ProductController::class, 'home']);
-//     Route::get('/baby-kid', [ProductController::class, 'baby']);
-// });
-
-// // 3. User
-// Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
-
-// // 4. Penjualan
-// Route::get('/sales', [SalesController::class, 'index']);
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
